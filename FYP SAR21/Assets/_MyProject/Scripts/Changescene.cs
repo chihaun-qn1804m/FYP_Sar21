3aebb17afbfbffb1eb95e63c8cee8f50282e2b16
Chi Haun <lochihaunbdjc@gmail.com>
2022-05-16 02:44:51 +0800

    improved AI

--- FYP SAR21/Assets/_MyProject/Scripts/Changescene.cs
+++ FYP SAR21/Assets/_MyProject/Scripts/Changescene.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Changescene : MonoBehaviour
 {
-   public GameObject room0;
-   public GameObject room1;
-   public GameObject room2;
+   public GameObject network;
 
     private void OnTriggerEnter(Collider other)
     {   
-        Debug.Log("test");
+    
+       network.GetComponent<NetworkManagerPhoton>().InitialiazeRoom(0, other);
     }
 }
