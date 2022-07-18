b5973d25cb581b28af38db34517223a0de0f631c
QuekShouZhi <SHOUZHI.20@ichat.sp.edu.sg>
2022-07-19 00:24:38 +0800

    fixing bug

--- FYP SAR21/Assets/BNG Framework/Scripts/Core/GrabbableChild.cs
+++ FYP SAR21/Assets/BNG Framework/Scripts/Core/GrabbableChild.cs
@@ -3,32 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-namespace BNG
-{
-    public class GrabbableChild : GrabbableEvents
-    {
-
+namespace BNG {
+    public class GrabbableChild : MonoBehaviour {
+        [Tooltip("The Parent Grabbable Object to be grabbed.")]
         public Grabbable ParentGrabbable;
-
-        public override void OnGrab(Grabber grabber)
-        {
-
-            base.OnGrab(grabber);
-
-            // Never hold this item
-            grab.DropItem(grabber, false, false);
-
-            // Don't grab this if we are currently grabbing / remote grabbing a different item
-            if (grabber.RemoteGrabbingItem || grabber.HoldingItem)
-            {
-                return;
-            }
-
-            // Grab the Parent Grabbable instead
-            if (ParentGrabbable != null && !ParentGrabbable.BeingHeld)
-            {
-                grabber.GrabGrabbable(ParentGrabbable);
-            }
-        }
     }
 }
